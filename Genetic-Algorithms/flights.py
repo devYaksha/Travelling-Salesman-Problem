0da604046d9f8f934f7374743fda9e9535af2355
devYaksha <gssantoz2012@gmail.com>
2023-08-15 11:24:12 -0300

    printCalendar func

--- Genetic-Algorithms/flights.py
+++ Genetic-Algorithms/flights.py
@@ -24,4 +24,23 @@ for linha in open('./Genetic-Algorithms/flights.txt', 'r'):
     flights.setdefault((origin, destiny), [])
     flights[(origin, destiny)].append((departureTime, arrivalTime, value))
 
-print(flights)
+def printCalendar(calendar):
+    fly_id = -1
+    totalPrice = 0
+    for i in range(len(calendar)//2):
+        name = people[i][0]
+        origin = people[i][1]
+        fly_id += 1
+        fly_departure = flights[(origin, Finaldestiny)][calendar[fly_id]]
+        print(name)
+        print(fly_departure)
+        totalPrice += int(fly_departure[2])
+        fly_id += 1
+        fly_back = flights[(destiny, origin)][calendar[fly_id]]
+        totalPrice += int(fly_back[2])
+        print('%10s%10s %5s-%5s U$%3s %5s-%5s U$%3s' % (name, origin, fly_departure[0], fly_back[1], fly_departure[2], 
+                                                    fly_back[0], fly_back[1], fly_back[2]))
+    print('Preço total: ', totalPrice)
+
+
+printCalendar([1,4, 3,2, 7,3, 6,3, 2,4, 5,3])
