976348936d5726661d00f511975f8acf2f51aca1
devYaksha <gssantoz2012@gmail.com>
2023-09-18 14:23:17 -0300

    utils comments

--- Genetic-Algorithms/H-CFS_PCV/CFS/utils.py
+++ Genetic-Algorithms/H-CFS_PCV/CFS/utils.py
@@ -1,10 +1,5 @@
 def in_array(value, vector):
-    """Verifica se o 'value' esta no 'vector'.
-    
-    Returns:
-        vectorType: Retorna um valor do tipo do vetor caso esteja na lista, ou -1 caso não esteja na lista
-    
-    """
+    """Verifica se o 'value' esta no 'vector'"""
     for i in range(len(vector)):
         if value == vector[i]:
             return i
@@ -21,17 +16,7 @@ def distinct(categorical):
     return vector_distinct
 
 def frequence_double(dist_class, vector_categorical):
-    """
-    Calcula a frequência de ocorrência de elementos do vetor categórico em relação a uma distribuição de classes.
-
-    Args:
-        dist_class (list): Uma lista que representa a distribuição de classes, contendo os valores possíveis.
-        vector_categorical (list): Uma lista de elementos categóricos para os quais a frequência será calculada.
-
-    Returns:
-        list: Uma lista contendo a frequência de ocorrência de cada elemento de 'vector_categorical' em relação a 'dist_class'.
-        
-    """
+    """Calcula a frequência de ocorrência de elementos do vetor categórico em relação a uma distribuição de classes."""
     frequence = [0 for i in range(len(dist_class))]
     for k in range(len(vector_categorical)):
         position = in_array(vector_categorical[k], dist_class)
